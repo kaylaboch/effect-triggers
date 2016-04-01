5494dbeeeeed2f68681068af401f9bd6844697ca
kaylaboch <boch.kayla57@gmail.com>
2016-04-01 15:15:11 -0400

    added a var and first function

--- js/main.js
+++ js/main.js
@@ -3,3 +3,8 @@ var $list = $('.list');
 var $move = $('.btn-move');
 var $bounce = $('.btn-bounce');
 var $collapse = $('.btn-collapse-expand');
+var $append = $('.btn-append');
+
+$btn.on('click', function (e) {
+  $('.box').toggleClass('box-show');
+});
